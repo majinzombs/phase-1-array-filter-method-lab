e5d613717148bf113b62b47eaf5d07bc385a8277
Jonathan Koban <jon.koban@outlook.com>
2022-04-10 23:29:47 -0400

    commit tree 973178b5a6abe11eff40169c1581e03a39a50ac7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    const driversNames = drivers.filter(driver => driver.toUpperCase() === name.toUpperCase())
+    return driversNames
+}
+
+function fuzzyMatch(drivers, name) {
+    const driverInitial = drivers.filter(driver => driver.charAt(0) === name.charAt(0))
+    return driverInitial
+}
+
+function matchName(drivers, string) {
+    const nameChecker = drivers.filter(driver => driver.name === string)
+    return nameChecker
+}
